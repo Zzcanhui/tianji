d35f105e03dc640d9d64276fa8eacef30c106023
zcanhui <qwer178162@163.com>
2026-01-22 11:40:46 +0800

    签到和积分功能初始化

---
+++ tj-learning/src/main/java/com/tianji/learning/mapper/PointsBoardMapper.java
@@ -0,0 +1,16 @@
+package com.tianji.learning.mapper;
+
+import com.tianji.learning.domain.po.PointsBoard;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 学霸天梯榜 Mapper 接口
+ * </p>
+ *
+ * @author canhui
+ * @since 2026-01-22
+ */
+public interface PointsBoardMapper extends BaseMapper<PointsBoard> {
+
+}
---
+++ tj-learning/src/main/java/com/tianji/learning/service/IPointsBoardSeasonService.java
@@ -0,0 +1,16 @@
+package com.tianji.learning.service;
+
+import com.tianji.learning.domain.po.PointsBoardSeason;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author canhui
+ * @since 2026-01-22
+ */
+public interface IPointsBoardSeasonService extends IService<PointsBoardSeason> {
+
+}
