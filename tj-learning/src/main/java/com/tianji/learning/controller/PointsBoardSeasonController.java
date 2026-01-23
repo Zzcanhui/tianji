cc7921ec1b05a83f0ecd526985942b9d78060b0b
zcanhui <qwer178162@163.com>
2026-01-23 17:27:34 +0800

    实现查询赛季列表功能

--- tj-learning/src/main/java/com/tianji/learning/controller/PointsBoardSeasonController.java
+++ tj-learning/src/main/java/com/tianji/learning/controller/PointsBoardSeasonController.java
@@ -1,20 +1,36 @@
 package com.tianji.learning.controller;
 
 
+import com.tianji.learning.domain.po.PointsBoardSeason;
+import com.tianji.learning.service.IPointsBoardSeasonService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
- *  前端控制器
+ * 赛季相关接口
  * </p>
  *
  * @author canhui
  * @since 2026-01-22
  */
 @RestController
-@RequestMapping("/points-board-season")
+@RequestMapping("/boards")
+@RequiredArgsConstructor
+@Api(tags = "赛季相关接口")
 public class PointsBoardSeasonController {
 
+    private final IPointsBoardSeasonService seasonService;
+
+    @GetMapping("/seasons/list")
+    @ApiOperation("查询赛季列表")
+    public List<PointsBoardSeason> querySeasonList() {
+        return seasonService.list();
+    }
 }
