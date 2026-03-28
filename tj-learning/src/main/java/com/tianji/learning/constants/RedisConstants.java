a94907be80d2e251ff47bd2e66bac70db4ca5064
zcanhui <qwer178162@163.com>
2026-03-28 09:58:49 +0800

    生成实时榜单

--- tj-learning/src/main/java/com/tianji/learning/constants/RedisConstants.java
+++ tj-learning/src/main/java/com/tianji/learning/constants/RedisConstants.java
@@ -1,8 +1,13 @@
 package com.tianji.learning.constants;
 
-public class RedisConstants {
+public interface RedisConstants {
     /*
      * 签到记录key的前缀:sign:uid:110:202301
      */
     public static final String SIGN_RECORD_KEY_PREFIX = "sign:uid:";
+
+    /*
+     * 积分排行榜key的前缀:boards:202301
+     */
+    public static final String POINTS_BOARD_KEY_PREFIX = "boards:";
 }
--- tj-learning/src/main/java/com/tianji/learning/service/impl/PointsRecordServiceImpl.java
+++ tj-learning/src/main/java/com/tianji/learning/service/impl/PointsRecordServiceImpl.java
@@ -10,6 +10,10 @@ import com.tianji.learning.enums.PointsRecordType;
 import com.tianji.learning.mapper.PointsRecordMapper;
 import com.tianji.learning.service.IPointsRecordService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.tianji.learning.constants.RedisConstants;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -27,39 +31,42 @@ import java.util.Map;
  * @since 2026-01-22
  */
 @Service
+@RequiredArgsConstructor
 public class PointsRecordServiceImpl extends ServiceImpl<PointsRecordMapper, PointsRecord> implements IPointsRecordService {
+
+    private final StringRedisTemplate redisTemplate;
 
     @Override
     public void addPointsRecord(Long userId, int points, PointsRecordType type) {
+        LocalDateTime now = LocalDateTime.now();
         int maxPoints = type.getMaxPoints();
-        // 1. 判断当前方式有没有积分上限
+        // 1.判断当前方式有没有积分上限
         int realPoints = points;
-        if (maxPoints>0) {
-            // 2. 有，则需要判断是否超过上限
-            LocalDateTime now = LocalDateTime.now();
+        if(maxPoints > 0) {
+            // 2.有，则需要判断是否超过上限
             LocalDateTime begin = DateUtils.getDayStartTime(now);
             LocalDateTime end = DateUtils.getDayEndTime(now);
-            // 2.1. 查询今日已得积分
+            // 2.1.查询今日已得积分
             int currentPoints = queryUserPointsByTypeAndDate(userId, type, begin, end);
-            // 2.2. 判断是否超过上限
-            if (currentPoints + points >= maxPoints) {
-                // 2.3. 超过，直接结束
+            // 2.2.判断是否超过上限
+            if(currentPoints >= maxPoints) {
+                // 2.3.超过，直接结束
                 return;
             }
-            // 2.4. 没超过，保存积分记录
-            if (currentPoints + points > maxPoints) {
-                // 2.4. 没超过，保存积分记录
+            // 2.4.没超过，保存积分记录
+            if(currentPoints + points > maxPoints){
                 realPoints = maxPoints - currentPoints;
             }
         }
-        // 3. 没有，直接保存积分记录
-        PointsRecord p = new PointsRecord()
-                .setUserId(userId)
-                .setPoints(realPoints)
-                .setType(type);
-       save(p);
-
-
+        // 3.没有，直接保存积分记录
+        PointsRecord p = new PointsRecord();
+        p.setPoints(realPoints);
+        p.setUserId(userId);
+        p.setType(type);
+        save(p);
+        // 4.更新总积分到Redis
+        String key = RedisConstants.POINTS_BOARD_KEY_PREFIX + now.format(DateUtils.POINTS_BOARD_SUFFIX_FORMATTER);
+        redisTemplate.opsForZSet().incrementScore(key, userId.toString(), realPoints);
     }
 
     @Override
